01817447aade03618c3b16f8a79deb167ad99c42
l0eb <mrloebdesanta@gmail.com>
2021-08-21 17:00:18 +0530

    initial commit

---
+++ Assets/Chaser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour {
+
+    public Transform TargetTransform;
+    public float speed = 7;
+    void Update() {
+        Vector3 Displacement = TargetTransform.position - transform.position;
+        Vector3 Direction = Displacement.normalized;
+        Vector3 velocity = Direction * speed;
+        float distance = Displacement.magnitude;
+        if (distance > 1.5f) {
+            transform.Translate(velocity * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 10;
+    void Update() {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+
+        transform.Translate(moveAmount);
+    }
+}
